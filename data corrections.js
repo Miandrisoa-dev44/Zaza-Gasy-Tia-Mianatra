993f52d326534f839f31961cd5569c3b687a3a34
Miandrisoa-dev44 <ratovonjanaharymiandrisoaderic@gmail.com>
2025-11-28 20:11:25 +0300

    Add data corrections for educational levels

---
+++ data corrections.js
@@ -0,0 +1,15 @@
+// REMPLACER PAR VOTRE NUMÉRO WHATSAPP DE SUPPORT (avec code pays, sans '+')
+const WHATSAPP_NUMBER = "261341108401"; // Ex: 261331234567
+
+const correctionsData = [
+    // Primaire
+    { niveau: "primaire", type: "Examen", matiere: "Maths", titre: "Correction Brevet Blanc Maths 2024", id: "P-BBM-24" },
+    { niveau: "primaire", type: "Concours", matiere: "Français", titre: "Correction Concours d'entrée 6ème - Français", id: "P-C6-FR" },
+    // Collège
+    { niveau: "college", type: "Examen", matiere: "SVT", titre: "Correction Brevet Session Août 2023 - SVT", id: "C-BSVA-23" },
+    { niveau: "college", type: "Série", matiere: "Physique", titre: "Correction Exercices Série 3 - 4ème", id: "C-S3-PH" },
+    // Lycée
+    { niveau: "lycee", type: "Examen", matiere: "Philosophie", titre: "Correction Bac Philo Série A - Sujet 1", id: "L-BPA-S1" },
+    { niveau: "lycee", type: "Concours", matiere: "Anglais", titre: "Correction Concours FOP 2024 - Anglais", id: "L-CFOP-24" },
+];
+     
